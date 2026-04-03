6388670995cbb077daa6a796333446bd5d1e0a42
Michael Mussato <michimussato@gmail.com>
2026-04-03 12:26:57 +0200

    fix import path

--- src/OpenStudioLandscapes/DagsterCodeLocation/ShotProcessor/definitions.py
+++ src/OpenStudioLandscapes/DagsterCodeLocation/ShotProcessor/definitions.py
@@ -1,6 +1,6 @@
 from dagster import Definitions, load_assets_from_modules
 
-from Dagster_ShotProcessor import assets  # noqa: TID252
+from OpenStudioLandscapes.DagsterCodeLocation.ShotProcessor import assets  # noqa: TID252
 
 all_assets = load_assets_from_modules([assets])
 
