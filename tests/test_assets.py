d68cf72cdcb569171d8d45344350a58d5c74bd5c
Michael Mussato <michimussato@gmail.com>
2026-04-07 12:05:08 +0200

    first unit test attempts

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -1,6 +1,7 @@
 # https://docs.dagster.io/guides/test/unit-testing-assets-and-ops#unit-test-examples
 import json
 import pathlib
+import shutil
 from typing import Generator, List, Any, Dict
 
 # https://docs.dagster.io/guides/test/unit-testing-assets-and-ops#upstream-dependencies
@@ -54,6 +55,32 @@ def test_raw_to_oiio() -> None:
 
     context = build_asset_context()
 
+    oiio_out = fixtures / "v123/oiio"
+
+    expected = [
+
+        {
+            'exr_touched_out': oiio_out / 'oiio_exr/sh030_001.1200.exr',
+            'overlay_handle_buf_out': oiio_out / 'oiio_overlay_handle/sh030_001.1200.exr',
+            'overlay_text_buf_out': oiio_out / 'oiio_overlay_text/sh030_001.1200.exr',
+        },
+        {
+            'exr_touched_out': oiio_out / 'oiio_exr/sh030_001.1201.exr',
+            'overlay_handle_buf_out': oiio_out / 'oiio_overlay_handle/sh030_001.1201.exr',
+            'overlay_text_buf_out': oiio_out / 'oiio_overlay_text/sh030_001.1201.exr',
+        },
+        {
+            'exr_touched_out': oiio_out / 'oiio_exr/sh030_001.1250.exr',
+            'overlay_handle_buf_out': oiio_out / 'oiio_overlay_handle/sh030_001.1250.exr',
+            'overlay_text_buf_out': oiio_out / 'oiio_overlay_text/sh030_001.1250.exr',
+        },
+        {
+            'exr_touched_out': oiio_out / 'oiio_exr/sh030_001.1251.exr',
+            'overlay_handle_buf_out': oiio_out / 'oiio_overlay_handle/sh030_001.1251.exr',
+            'overlay_text_buf_out': oiio_out / 'oiio_overlay_text/sh030_001.1251.exr',
+        },
+    ]
+
     with open(fixtures / "kitsu_task_dict.json") as fr:
         kitsu_task_dict = json.load(fr)
 
@@ -69,18 +96,23 @@ def test_raw_to_oiio() -> None:
         fixtures / "v123/raw/sh030_001.1251.exr",
     ]
 
-    asset_return_generator: Generator[Output[pathlib.Path] | AssetMaterialization | Any, Any, None] = raw_to_oiio(
-        context=context,
-        get_kitsu_task_dict=kitsu_task_dict,
-        version="v123",
-        render_version_directory=fixtures,
-        CONFIG=config,
-        CONFIG_OIIO=config_oiio,
-        image_sequence=image_sequence,
+    # asset_return_generator: Generator[Output[pathlib.Path] | AssetMaterialization | Any, Any, None] = list(raw_to_oiio(
+    result = list(
+        raw_to_oiio(
+            context=context,
+            get_kitsu_task_dict=kitsu_task_dict,
+            version="v123",
+            render_version_directory=fixtures,
+            CONFIG=config,
+            CONFIG_OIIO=config_oiio,
+            image_sequence=image_sequence,
+        )
     )
 
-    result = [i for i in asset_return_generator]
+    shutil.rmtree(oiio_out)
 
     output: Output = result[0]
     actual = output.value
     asset_materialization: AssetMaterialization = result[1]
+
+    assert actual == expected
