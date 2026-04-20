24ca2b32d4e2a38b2a0552696ab94990bb1d69a8
Michael Mussato <michimussato@gmail.com>
2026-04-20 12:36:55 +0200

    add dependency on the main render job

--- src/OpenStudioLandscapes/DagsterCodeLocation/ShotProcessor/base/sensors/auto_materialize_sensor.py
+++ src/OpenStudioLandscapes/DagsterCodeLocation/ShotProcessor/base/sensors/auto_materialize_sensor.py
@@ -2,11 +2,17 @@ from dagster import (
     DefaultSensorStatus,
     AssetSelection,
     AutomationConditionSensorDefinition,
+    # AssetKey,
 )
+
+# from OpenStudioLandscapes.DagsterCodeLocation.JobProcessor.dagster_job_processor.assets.read_yaml import (
+#     # ASSET_HEADER_JOB_PROCESSOR,
+#     ASSET_HEADER_JOB_PROCESSOR_DEADLINE,
+# )
 
 shot_processor_auto_materialize_sensor = AutomationConditionSensorDefinition(
     "ShotProcessor_AutoMaterializeSensor",
-    target=AssetSelection.all(include_sources=True),
+    target=AssetSelection.all(),
     minimum_interval_seconds=15,
-    default_status=DefaultSensorStatus.RUNNING,
+    default_status=DefaultSensorStatus.STOPPED,
 )
